0ef7c4ea1f050cd48dde32132595960b265e0268
santiagofelix3 <santiagofelixc@Santiagos-MacBook-Pro.local>
2017-04-06 11:26:48 -0600

    Analytics

--- ONtheRoad/ONtheRoad/AnalyticsViewController.swift
+++ ONtheRoad/ONtheRoad/AnalyticsViewController.swift
@@ -7,79 +7,233 @@
 //
 
 import UIKit
+import Charts
 
 class AnalyticsViewController: UIViewController {
     
-    @IBOutlet weak var badCircle: KDCircularProgress!
-    @IBOutlet weak var averageCircle: KDCircularProgress!
-    @IBOutlet weak var goodCircle: KDCircularProgress!
-    @IBOutlet weak var lowCircle: KDCircularProgress!
+    @IBOutlet weak var pieChartCiew: PieChartView!
+    @IBOutlet weak var barChartView: BarChartView!
+    @IBOutlet weak var lineChartView: LineChartView!
     
-    var badTime = 10.0
-    var averageTime = 25.0
-    var goodTime = 20.0
-    var lowTime = 5.0
+    var kilometers: [String]!
+    var counter = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupAnimation()
-        updateCircle()
+        let kilometers1 = ["20", "13", "12", "8", "10", "7", "12", "11", "15", "7"]
+        let kilometers2 = ["9", "8", "10", "7", "10", "12", "12", "11", "7", "10", "9", "8", "3", "2", "3", "4", "5", "4", "6", "6"]
+        let efficiency = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+        
+        setBackground()
+        updatePieChartData()
+        updateBarChartData(dataPoints: kilometers1, values: efficiency)
+        updateLineChartData(dataPoints: kilometers2, values: efficiency)
+
+        //setChart(dataPoints: kilometers, values: efficiency)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Functions
+    
+    func updatePieChartData()  {
+        
+        //let chart = PieChartView(frame: self.view.frame)
+        
+        let track = ["Low", "Good", "Average", "Bad"]
+        let money = [10, 20, 22, 12]
+        
+        var entries = [PieChartDataEntry]()
+        for (index, value) in money.enumerated() {
+            let entry = PieChartDataEntry()
+            entry.y = Double(value)
+            entry.label = track[index]
+            entries.append(entry)
+        }
+        
+        let set = PieChartDataSet( values: entries, label: "")
+        
+        var colors: [UIColor] = []
+        
+        for i in 0..<money.count {
+            if i == 0 {
+                let red = Double(176)
+                let green = Double(229)
+                let blue = Double(251)
+                let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+                colors.append(color)
+            } else if i == 1 {
+                let red = Double(30)
+                let green = Double(159)
+                let blue = Double(62)
+                let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+                colors.append(color)
+            } else if i == 2 {
+                let red = Double(224)
+                let green = Double(127)
+                let blue = Double(21)
+                let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+                colors.append(color)
+            } else {
+                let red = Double(219)
+                let green = Double(51)
+                let blue = Double(55)
+                let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+                colors.append(color)
+            }
+        }
+        
+        set.colors = colors
+        pieChartCiew.backgroundColor = .clear
+
+        let data = PieChartData(dataSet: set)
+        pieChartCiew.data = data
+        pieChartCiew.noDataText = "No data available"
+        // user interaction
+        pieChartCiew.isUserInteractionEnabled = false
+        
+        //let description = Description()
+        //description.text = "iOSCharts.io"
+        //pieChartCiew.chartDescription = description
+        pieChartCiew.centerText = "Total Trip Time"
+        pieChartCiew.holeRadiusPercent = 0.7
+        pieChartCiew.transparentCircleColor = UIColor.clear
+        //self.view.addSubview(pieChartCiew)
+        
     }
     
-    //func newAngle() -> Float {
-    //    return Float(360 * (currentCount / maxCount))
-    // }
     
-    func updateCircle() {
-        let totalTime = badTime + averageTime + goodTime + lowTime
+    func updateBarChartData(dataPoints: [String], values: [String]) {
         
-        var newAngle = Float(360 * (badTime / totalTime))
-        badCircle.animate(toAngle: Double(newAngle), duration: 1, completion: nil)
-        print(newAngle)
+        barChartView.noDataText = "You need to provide data for the chart."
         
-        var nextAngle = Double(newAngle)
-        print(nextAngle)
-        averageCircle.startAngle = nextAngle
-        newAngle = Float(360 * (averageTime / totalTime))
-        averageCircle.animate(toAngle: Double(newAngle), duration: 1, completion: nil)
-        print(newAngle)
+        var dataEntries: [BarChartDataEntry] = []
         
-        nextAngle += Double(newAngle)
-        print(nextAngle)
-        goodCircle.startAngle = nextAngle
-        newAngle = Float(360 * (goodTime / totalTime))
-        goodCircle.animate(toAngle: Double(newAngle), duration: 1, completion: nil)
-        print(newAngle)
+        for (i, j) in dataPoints.enumerated() {
+            let dataEntry = BarChartDataEntry()
+            dataEntry.x = Double(i)
+            dataEntry.y = Double(j)!
+            
+            dataEntries.append(dataEntry)
+        }
         
-        nextAngle += Double(newAngle)
-        print(nextAngle)
-        lowCircle.startAngle = nextAngle
-        newAngle = Float(360 * (lowTime / totalTime))
-        lowCircle.animate(toAngle: Double(newAngle), duration: 1, completion: nil)
-        print(newAngle)
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Efficiency")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        let targetLine = ChartLimitLine(limit: 8.0, label: "Ideal")
+        
+        targetLine.lineWidth = 1
+        targetLine.valueTextColor = UIColor.white
+        
+        let noZeroFormatter = NumberFormatter()
+        noZeroFormatter.zeroSymbol = ""
+        chartDataSet.valueFormatter = DefaultValueFormatter(formatter: noZeroFormatter)
+        
+        barChartView.data = chartData
+        barChartView.chartDescription?.text = ""
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.xAxis.labelTextColor = Palette.InfoText
+        barChartView.xAxis.drawLabelsEnabled = false
+        barChartView.leftAxis.labelTextColor = Palette.InfoText
+        barChartView.rightAxis.drawGridLinesEnabled = false
+        barChartView.xAxis.drawGridLinesEnabled = false
+        
+        barChartView.leftAxis.addLimitLine(targetLine)
+        barChartView.leftAxis.axisMaximum = 30.0
+        barChartView.rightAxis.axisMinimum = 0.0
+        barChartView.rightAxis.axisMaximum = 30.0
+        barChartView.leftAxis.axisMinimum = 0.0
+        
+        barChartView.backgroundColor = UIColor.clear
+        barChartView.leftAxis.drawLabelsEnabled = true
+        barChartView.rightAxis.drawLabelsEnabled = false
+        barChartView.leftAxis.axisLineColor = UIColor.clear
+        barChartView.rightAxis.axisLineColor = UIColor.clear
+        barChartView.xAxis.axisLineColor = UIColor.clear
+        barChartView.legend.enabled = false
+        
+        chartDataSet.valueTextColor = .clear
+        chartDataSet.setColor(.white, alpha: 0.8)
+        chartDataSet.highlightEnabled = false
     }
     
-    func setupAnimation() {
-        badCircle.angle = 0
-        averageCircle.angle = 0
-        goodCircle.angle = 0
-        lowCircle.angle = 0
+    func setBackground() {
+        let topColor = UIColor(red: 99/255.0, green: 175/255.0, blue: 213/255.0, alpha: 1.0)
+        let bottomColor = UIColor(red: 5/255.0, green: 54/255.0, blue: 106/255.0, alpha: 1.0)
+        
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [Float] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations as [NSNumber]?
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    func updateLineChartData(dataPoints: [String], values: [String]) {
+        lineChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for (i, j) in dataPoints.enumerated() {
+            let dataEntry = BarChartDataEntry()
+            dataEntry.x = Double(i)
+            dataEntry.y = Double(j)!
+            
+            dataEntries.append(dataEntry)
+        }
+        
+        let targetLine = ChartLimitLine(limit: 8.0, label: "Ideal")
+        
+        targetLine.lineWidth = 1
+        targetLine.valueTextColor = UIColor.white
+        
+        let noZeroFormatter = NumberFormatter()
+        noZeroFormatter.zeroSymbol = ""
+        
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Efficiency")
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        
+        lineChartDataSet.valueFormatter = DefaultValueFormatter(formatter: noZeroFormatter)
+        
+        lineChartView.data = lineChartData
+        lineChartView.chartDescription?.text = ""
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.xAxis.labelTextColor = Palette.InfoText
+        lineChartView.xAxis.drawLabelsEnabled = false
+        lineChartView.leftAxis.labelTextColor = Palette.InfoText
+        //lineChartView.rightAxis.drawLabelsEnabled = false
+        lineChartView.rightAxis.drawGridLinesEnabled = false
+        lineChartView.xAxis.drawGridLinesEnabled = false
+        
+        lineChartView.leftAxis.addLimitLine(targetLine)
+        lineChartView.leftAxis.axisMaximum = 30.0
+        lineChartView.rightAxis.axisMinimum = 0.0
+        lineChartView.rightAxis.axisMaximum = 30.0
+        lineChartView.leftAxis.axisMinimum = 0.0
+        
+        lineChartView.backgroundColor = UIColor.clear
+        lineChartView.leftAxis.drawLabelsEnabled = true
+        lineChartView.rightAxis.drawLabelsEnabled = false
+        lineChartView.leftAxis.axisLineColor = UIColor.clear
+        lineChartView.rightAxis.axisLineColor = UIColor.clear
+        lineChartView.xAxis.axisLineColor = UIColor.clear
+        lineChartView.legend.enabled = false
+        
+        lineChartDataSet.setCircleColor(UIColor.clear)
+        lineChartDataSet.drawCircleHoleEnabled = true
+        lineChartDataSet.circleHoleColor = Palette.InfoText
+        lineChartDataSet.circleHoleRadius = 3.0
+        lineChartDataSet.valueTextColor = .clear
+        lineChartDataSet.setColor(UIColor.white.withAlphaComponent(0.5))
+        lineChartDataSet.lineWidth = 2
+        
+        
+        let gradientColors = [UIColor.white.cgColor, UIColor.clear.cgColor] as CFArray // Colors of the gradient
+        let colorLocations:[CGFloat] = [1.0, 0.0] // Positioning of the gradient
+        let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) // Gradient Object
+        lineChartDataSet.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0) // Set the Gradient
+        lineChartDataSet.drawFilledEnabled = true
+    }
+
 }
